3bda02d55c8a24a303aaa85af1b12330ca1e7b38
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2021-08-30 18:37:10 -0300

    Agrego parcial hogwarts

---
+++ casas-de-hogwarts/CasasDeHogwarts.pl
@@ -0,0 +1,10 @@
+
+casa(ravenclaw).
+casa(slytherin).
+casa(hufflepuff).
+casa(gryffindor).
+
+% mago(Nombre, TipoSangre, Caracteristicas, CasaQueOdia)
+mago(harry, mestizo, [corajudo, amistoso, orgulloso, inteligente], slytherin).
+mago(draco, pura, [inteligente, orgulloso], hufflepuff).
+mago(hermione, impura, [inteligente, orgulloso, responsable], _).
---
+++ casas-de-hogwarts/README.md
@@ -0,0 +1,4 @@
+# Casas de Hogwarts
+
+El link del parcial se encuentra [aquí](https://docs.google.com/document/d/e/2PACX-1vR9SBhz2J3lmqcMXOBs1BzSt7N1YWPoIuubAmQxPIOcnbn5Ow9REYt4NXQzOwXXiUaEQ4hfHNEt3_C7/pub)
+
