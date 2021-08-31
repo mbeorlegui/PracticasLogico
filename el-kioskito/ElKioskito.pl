4f668fbf015b3e6dc0d0433bf82d3da4b08b1cad
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2021-08-31 13:08:39 -0300

    Hasta ultimo punto

---
+++ el-kioskito/ElKioskito.pl
@@ -0,0 +1,79 @@
+
+% atiende(Profesor, Dia, HorarioApertura, HorarioCierre)
+atiende(dodain, lunes, 9, 15).
+atiende(dodain, miercoles, 9, 15).
+atiende(dodain, viernes, 9, 15).
+atiende(juanC, sabados, 18, 22).
+atiende(juanC, domingo, 18, 22).
+atiende(lucas, martes, 10, 20).
+atiende(juanFdS, jueves, 10, 20).
+atiende(juanFdS, viernes, 12, 20).
+atiende(leoC, lunes, 14, 18).
+atiende(leoC, miercoles, 14, 18).
+atiende(martu, miercoles, 23, 24).
+
+% 1
+atiende(vale, Dia, HorarioApertura, HorarioCierre):-
+  atiende(dodain, Dia, HorarioApertura, HorarioCierre).
+atiende(vale, Dia, HorarioApertura, HorarioCierre):-
+  atiende(juanC, Dia, HorarioApertura, HorarioCierre).
+/*
+  Solamente ponemos lo de vale, ya que en logico
+  solamente ponemos predicados y clausulas con 
+  las cosas que estamos seguros que son verdaderas
+  no contempla ambigüedades en ese sentido.
+  El concepto que interviene es el "Principio 
+  de universo cerrado"
+*/
+
+
+% 2 quienAtiendeElKiosko(Dia, Hora, Persona)
+quienAtiende(Dia, Horario, Persona):-
+  atiende(Persona, Dia, HorarioApertura, HorarioCierre),
+  between(HorarioApertura, HorarioCierre, Horario).
+
+% 3. foreverAlone(Persona, Dia, Horario) -> Saber si en ese horario atiende solo el kioskito (debe usar not/1)
+foreverAlone(Persona, Dia, Horario):-
+  quienAtiende(Dia, Horario, Persona),
+  not((quienAtiende(Dia, Horario, OtraPersona), Persona \= OtraPersona)).
+% Una persona atiende sola en un dia y horario si:
+% no hay otra persona distinta que atienda en ese dia y horario
+
+
+% 4. posibilidadesDeAtencion(Dia, Persona) -> Este tema no lo vimos en clase
+% golosinas(ValorEnPlata)
+% cigarrillos([MarcasDeCigarrillos])
+% bebidas(Tipo, Cantidad) -> Tipo: alcoholica / noAlcoholica
+
+% venta(Persona, fecha(Dia, Mes), [LoQueVendio])
+venta(dodain, fecha(10, 8), [golosinas(1200), cigarrillos([jockey]), golosinas(50)]).
+venta(dodain, fecha(18, 8), [bebidas(alcoholica, 8), bebidas(noAlcoholica, 1), golosinas(10)]).
+venta(martu, fecha(12, 8), [golosinas(1000), cigarrillos([chesterfield, colorado, parisiennes])]).
+venta(lucas, fecha(11, 8), [golosinas(600)]).
+venta(lucas, fecha(18, 8), [bebidas(noAlcoholica, 2), cigarrillos([derby])]).
+
+vendedor(Persona):- venta(Persona, _, _).
+
+vendedorEsSuertudo(Persona):-
+  vendedor(Persona),
+  forall(Dia, hizoUnaVentaImportante(Persona, Dia)).
+
+hizoUnaVentaImportante(Persona, Dia):-
+  venta(Persona, Dia, LoVendido),
+  primeraVentaFueImportante(LoVendido).
+
+primeraVentaFueImportante(LoQueVendio):-
+  nth0(0, LoQueVendio, PrimeraVenta),
+  esVentaImportante(PrimeraVenta).
+
+esVentaImportante(golosinas(ValorEnPlata)):-
+  ValorEnPlata > 100.
+
+esVentaImportante(cigarrillos(CigarrillosVendidos)):-
+  length(CigarrillosVendidos, DistintosTiposDeCigarrillo),
+  DistintosTiposDeCigarrillo > 2.
+
+
+esVentaImportante(bebidas(alcoholica, _)).
+esVentaImportante(bebidas(_, CantidadVendida)):-
+  CantidadVendida > 5.
---
+++ el-kioskito/README.md
@@ -0,0 +1,3 @@
+# El kioskito
+
+El link del parcial se encuentra [aquí](https://docs.google.com/document/d/1RNgFMlSqOKiwe9SEi1U2cQjCmdFfWNflqycSfp7Qa-w/edit#heading=h.8z5fk89ui0rg)
